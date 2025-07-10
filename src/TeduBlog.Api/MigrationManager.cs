18d6772196944c77a5194c592321e7015b1d811f
F88\sangnt3 <sangnt3@f88.vn>
2025-07-10 14:14:38 +0700

    2.9_seeding_db

---
+++ src/TeduBlog.Api/MigrationManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeduBlog.Data;
+
+namespace TeduBlog.Api
+{
+    public static class MigrationManager
+    {
+        public static WebApplication MigrateDatabase(this WebApplication app)
+        {
+            using(var scope = app.Services.CreateScope())
+            {
+                using (var context = scope.ServiceProvider.GetRequiredService<TeduBlogContext>())
+                {
+                    // auto migate db when add-migration, don't need to update-database
+                    context.Database.Migrate();
+                    new DataSeeder().SeedAsync(context).Wait();
+                }
+            }
+            return app;
+        }
+    }
+}
